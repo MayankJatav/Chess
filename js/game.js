6333a47db6ecfcef2214b77662ecb510f30c20d9
Mayank Jatav <M4sDevs@GMail.Com>
2022-11-12 00:32:52 +0530

    Added game.js for storing game related code

---
+++ js/game.js
@@ -0,0 +1,12 @@
+export class init {
+    constructor() {
+        this.chessBoard = [["rookb", "knightb", "bishopb", "queenb", "kingb", "bishopb", "knightb", "rookb"],
+                           ["pawnb", "pawnb",   "pawnb",   "pawnb",  "pawnb", "pawnb",   "pawnb",   "pawnb"],
+                           ["",      "",        "",        "",       "",       "",       "",        ""     ],
+                           ["",      "",        "",        "",       "",       "",       "",        ""     ],
+                           ["",      "",        "",        "",       "",       "",       "",        ""     ],
+                           ["",      "",        "",        "",       "",       "",       "",        ""     ],
+                           ["pawnw", "pawnw",   "pawnw",   "pawnw",  "pawnw", "pawnw",   "pawnw",   "pawnw"],
+                           ["rookw", "knightw", "bishopw", "queenw", "kingw", "bishopw", "knightw", "rookw"]];
+    }
+}   

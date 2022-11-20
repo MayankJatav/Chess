dcfc486385e647b3e1089ee8b0a75db3c043de22
Mayank Jatav <M4sDevs@GMail.Com>
2022-11-20 22:44:15 +0530

    Created Functions For Moves

--- js/game.js
+++ js/game.js
@@ -1,18 +1,109 @@
 export class moves {
     constructor() {
-        this.king = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]];
-        this.bishop = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+        this.chessBoard = [["rookb", "knightb", "bishopb", "queenb", "kingb", "bishopb", "knightb", "rookb"],
+        ["pawnb", "pawnb", "pawnb", "pawnb", "pawnb", "pawnb", "pawnb", "pawnb"],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["pawnw", "pawnw", "pawnw", "pawnw", "pawnw", "pawnw", "pawnw", "pawnw"],
+        ["rookw", "knightw", "bishopw", "queenw", "kingw", "bishopw", "knightw", "rookw"]];
+    }
+    getInBoundaryMoves(pieceMoves, currentPosition) {
+        let moves = [];
+        for (let i = 0; i < pieceMoves.length; i++) {
+            let a = currentPosition[0] + pieceMoves[i][0];
+            let b = currentPosition[1] + pieceMoves[i][1];
+            if (!(a > 7) && !(a < 0) && !(b > 7) && !(b < 0)) {
+                let arr = new Array();
+                arr.push(a, b);
+                moves.push(arr);
+            }
+        }
+        return moves;
+    }
+    getKingMoves(currentPosition) {
+        let currentPosPiece = this.chessBoard[currentPosition[0]][currentPosition[1]];
+        if (currentPosPiece != "kingw" && currentPosPiece != "kingb") { return []; }
+        let KingMoves = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]];
+        let moves = this.getInBoundaryMoves(KingMoves, currentPosition);
+        return moves;
+    }
+    getBishopMoves(currentPosition) {
+        let currentPosPiece = this.chessBoard[currentPosition[0]][currentPosition[1]];
+        if (currentPosPiece != "bishopw" && currentPosPiece != "bishopb") { return []; }
+        let bishopMoves = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
         [-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7],
         [-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-6, 6], [-7, 7],
         [1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7]];
-        this.knight = [[2, 1], [1, 2], [1, -2], [2, -1], [-2, 1], [-1, 2], [-1, -2], [-2, -1]];
-        this.rook = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
+        let moves = this.getInBoundaryMoves(bishopMoves, currentPosition);
+        return moves;
+    }
+    getKnightMoves(currentPosition) {
+        let currentPosPiece = this.chessBoard[currentPosition[0]][currentPosition[1]];
+        if (currentPosPiece != "knightw" && currentPosPiece != "knightb") { return []; }
+        let knightMoves = [[2, 1], [1, 2], [1, -2], [2, -1], [-2, 1], [-1, 2], [-1, -2], [-2, -1]];
+        let moves = this.getInBoundaryMoves(knightMoves, currentPosition);
+        return moves;
+    }
+    getRookMoves(currentPosition) {
+        let currentPosPiece = this.chessBoard[currentPosition[0]][currentPosition[1]];
+        if (currentPosPiece != "rookw" && currentPosPiece != "rookb") { return []; }
+        let rookMoves = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
         [0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7],
         [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0],
         [-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0], [-6, 0], [-7, 0]];
-        this.queen = this.bishop.concat(this.rook);
-        this.pawn = [[1, 0]];
-        this.pawn1 = [[-1, 0]];
+        let moves = this.getInBoundaryMoves(rookMoves, currentPosition);
+        return moves;
+    }
+    getQueenMoves(currentPosition) {
+        let currentPosPiece = this.chessBoard[currentPosition[0]][currentPosition[1]];
+        if (currentPosPiece != "queenw" && currentPosPiece != "queenb") { return []; }
+        let queenMoves = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+        [-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7],
+        [-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-6, 6], [-7, 7],
+        [1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7],
+        [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
+        [0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7],
+        [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0],
+        [-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0], [-6, 0], [-7, 0]];
+        let moves = this.getInBoundaryMoves(queenMoves, currentPosition);
+        return moves;
+    }
+    getPawnMoves(currentPosition) {
+        let currentPosPiece = this.chessBoard[currentPosition[0]][currentPosition[1]];
+        if (currentPosPiece != "pawnw" && currentPosPiece != "pawnb") { return []; }
+        let pawnMoves = [[1, 0]];
+        let pawnMoves1 = [[-1, 0]];
+        let moves;
+        if (currentPosPiece.charAt(currentPosPiece.length - 1) == "b") {
+            moves = this.getInBoundaryMoves(pawnMoves, currentPosition);
+        }
+        else {
+            moves = this.getInBoundaryMoves(pawnMoves1, currentPosition);
+        }
+        return moves;
+    }
+    getPossibleMovesCells(currentPos) {
+        let row = currentPos[0];
+        let col = currentPos[1];
+        let currentPosPiece = this.chessBoard[row][col];
+        let l = currentPosPiece.length;
+        let currentPiece = currentPosPiece.substring(0, l - 1);
+        switch (currentPiece) {
+            case "king":
+                return this.getKingMoves([row, col]);
+            case "queen":
+                return this.getQueenMoves([row, col]);
+            case "bishop":
+                return this.getBishopMoves([row, col]);
+            case "knight":
+                return this.getKnightMoves([row, col]);
+            case "rook":
+                return this.getRookMoves([row, col]);
+            case "pawn":
+                return this.getPawnMoves([row, col]);
+        }
     }
 }
 export class init {
--- js/js.js
+++ js/js.js
@@ -58,41 +58,8 @@ function setCellClickAction() {
     }
 }
 function glowCells(movesArray, currentPos) {
+    movesArray = movesObj.getPossibleMovesCells(currentPos);
     for (let i = 0; i < movesArray.length; i++) {
-        let a = currentPos[0] + movesArray[i][0];
-        let b = currentPos[1] + movesArray[i][1];
-        if (!(a > 7) && !(a < 0) && !(b > 7) && !(b < 0)) {
-            board[a][b].style.backgroundColor = "blue";
-        }
-    }
-}
-function glowPossibleMovesCells(currentPos) {
-    let row = currentPos[0];
-    let col = currentPos[1];
-    let currentPosPiece = inititilize.chessBoard[row][col];
-    let l = currentPosPiece.length;
-    let currentPiece = currentPosPiece.substring(0, l - 1);
-    switch (currentPiece) {
-        case "king":
-            glowCells(movesObj.king, [row, col]);
-            break;
-        case "queen":
-            glowCells(movesObj.queen, [row, col]);
-            break;
-        case "bishop":
-            glowCells(movesObj.bishop, [row, col]);
-            break;
-        case "knight":
-            glowCells(movesObj.knight, [row, col]);
-            break;
-        case "rook":
-            glowCells(movesObj.rook, [row, col]);
-            break;
-        case "pawn":
-            if (currentPosPiece.charAt(l - 1) == "b")
-                glowCells(movesObj.pawn, [row, col]);
-            else
-                glowCells(movesObj.pawn1, [row, col]);
-            break;
+            board[movesArray[i][0]][movesArray[i][1]].style.backgroundColor = "blue";
     }
 }
